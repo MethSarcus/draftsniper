b15cba4671c57742ede43c8c8c96939fd17aa9d7
Seth Marcus <seth@smarcus.dev>
2022-05-23 12:44:17 -0400

    Fixed ts issues

--- pages/api/picks.ts
+++ pages/api/picks.ts
@@ -8,9 +8,9 @@ const cors = Cors({
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
     return new Promise((resolve, reject) => {
-        fn(req, res, (result) => {
+        fn(req, res, (result: any) => {
         if (result instanceof Error) {
             return reject(result)
         }
