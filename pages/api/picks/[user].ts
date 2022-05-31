aa63e52a69f8570930ee9c58d9951819661a8b77
Seth Marcus <seth@smarcus.dev>
2022-05-31 14:40:49 -0400

    Fixed api calls to ensure no stale requests

--- pages/api/picks/[user].ts
+++ pages/api/picks/[user].ts
@@ -1,46 +1,89 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import Cors from 'cors'
-import { LeagueSettings } from '../../../interfaces/sleeper_api/LeagueSettings'
+import type { NextApiRequest, NextApiResponse } from "next"
+import Cors from "cors"
+import { LeagueSettings } from "../../../interfaces/sleeper_api/LeagueSettings"
+import { DraftPick } from "../../../interfaces/sleeper_api/DraftPick"
 
 const cors = Cors({
-    methods: ['GET', 'HEAD'],
-  })
+	methods: ["GET", "HEAD"],
+})
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
-    return new Promise((resolve, reject) => {
-        fn(req, res, (result: any) => {
-        if (result instanceof Error) {
-            return reject(result)
-        }
+function runMiddleware(
+	req: NextApiRequest,
+	res: NextApiResponse,
+	fn: Function
+) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result: any) => {
+			if (result instanceof Error) {
+				return reject(result)
+			}
 
-        return resolve(result)
-        })
-    })
+			return resolve(result)
+		})
+	})
 }
 
 type Data = {
-  picks: any
+	picks: DraftPick[]
 }
 
-export default async function handler(req: NextApiRequest,res: NextApiResponse<Data>) {
-    const { user } = req.query
-    await runMiddleware(req, res, cors)
-    
-    fetch(`https://api.sleeper.app/v1/user/${user}/drafts/nfl/2022`).then(response => response.json()).then((data: LeagueSettings[]) => {
-        const draftIds = data.map((league) => { return league.draft_id })
-        //res.status(200).json({ userId: draftIds })
-        Promise.all(data.map((league) => { return makeDraftPromise(league.draft_id)})).then((promData) => res.status(200).json({ picks: promData.flat() }))
-        
-    });
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) {
+	const { user } = req.query
+	await runMiddleware(req, res, cors)
 
-    ///res.status(200).json({ userId: `${user}` })
+	const picks = await loadPicks(user.toString())
+	res.status(200).json({ picks: picks.flat() })
 }
 
-function makeDraftPromise(draftId: string) {
-    return new Promise((resolve, reject) => {
-        fetch(`https://api.sleeper.app/v1/draft/${draftId}/picks`).then(response => resolve(response.json()))
-      })
+// -------------------------------------------------------------------
+// these functions retrieve all draft picks for all a user
+// -------------------------------------------------------------------
+function getDrafts(userId: string) {
+	// get drafts a user has participated in
+	return new Promise((resolve) => {
+		setTimeout(
+			() =>
+				resolve(
+					fetch(
+						`https://api.sleeper.app/v1/user/${userId}/drafts/nfl/2022`
+					).then((response) => response.json())
+				),
+			200
+		)
+	})
+}
+
+function getPicks(draftId: string) {
+	// get draft picks for the given draft id
+	return new Promise((resolve) => {
+		setTimeout(
+			() =>
+				resolve(
+					fetch(`https://api.sleeper.app/v1/draft/${draftId}/picks`)
+				),
+			200
+		)
+	})
+}
+
+async function loadPicks(userId: string) {
+	const drafts = await getDrafts(userId)
+
+	// instead of awaiting this call, create an array of Promises
+	const promises = (drafts as any).map((draftSettings: LeagueSettings) => {
+		return getPicks(draftSettings.draft_id).then((picks) => {
+			return (picks as any).json()
+		})
+	})
+
+	// use await on Promise.all so the Promises execute in parallel
+	const draftPicks = await Promise.all(promises)
+
+	return draftPicks
 }
