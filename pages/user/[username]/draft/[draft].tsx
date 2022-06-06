006382f3c9a9cc75e9ccc81d019b0651ef564e0d
Seth Marcus <seth@smarcus.dev>
2022-06-06 09:36:10 -0400

    Deleted test draft pick

--- pages/user/[username]/draft/[draft].tsx
+++ pages/user/[username]/draft/[draft].tsx
@@ -12,29 +12,6 @@ const DraftBoard = () => {
     "https://api.sleeper.app/v1/draft/" + router.query.draft + "/picks",
     fetcher
   );
-  const draftPick = {
-    round: 1,
-    roster_id: 4,
-    player_id: "8155",
-    picked_by: "376111571866267648",
-    pick_no: 1,
-    metadata: {
-      years_exp: "0",
-      team: "NYJ",
-      status: "Active",
-      sport: "nfl",
-      position: "RB",
-      player_id: "8155",
-      number: "0",
-      news_updated: "1651275628504",
-      last_name: "Hall",
-      injury_status: "",
-      first_name: "Breece",
-    },
-    is_keeper: null,
-    draft_slot: 1,
-    draft_id: "784515345380663297",
-  };
 
   if (error) return <div>Failed to load</div>;
   if (!data) return <div>Loading...</div>;
