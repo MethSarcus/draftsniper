fb34245f033c84d5dd668e89b5f68b61fafce716
Seth Marcus <seth@smarcus.dev>
2022-06-07 19:40:27 -0400

    prettified

--- pages/user/[username]/draft/[draft].tsx
+++ pages/user/[username]/draft/[draft].tsx
@@ -1,36 +1,39 @@
-import { useRouter } from "next/router";
-import useSWR from "swr";
-import axios from "axios";
-import DraftPickCard from "../../../../components/DraftPickCard";
-import { DraftPick } from "../../../../interfaces/sleeper_api/DraftPick";
-import { Grid, GridItem } from "@chakra-ui/react";
+import { useRouter } from "next/router"
+import useSWR from "swr"
+import axios from "axios"
+import DraftPickCard from "../../../../components/DraftPickCard"
+import { DraftPick } from "../../../../interfaces/sleeper_api/DraftPick"
+import { Grid, GridItem } from "@chakra-ui/react"
 
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+const fetcher = (url: string) => axios.get(url).then((res) => res.data)
 const DraftBoard = () => {
-  const router = useRouter();
-  const { data, error } = useSWR(
-    "https://api.sleeper.app/v1/draft/" + router.query.draft + "/picks",
-    fetcher
-  );
+	const router = useRouter()
+	const { data, error } = useSWR(
+		"https://api.sleeper.app/v1/draft/" + router.query.draft + "/picks",
+		fetcher
+	)
 
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
-  return (
-    <div>
-      <h1>DraftBoard: {router.query.draft}</h1>
-      <div>
-        <Grid
-          h="200px"
-          templateColumns="repeat(12, 1fr)"
-          gap={1}
-        >{data.map((pick: DraftPick) => {
-            return <GridItem key={pick.draft_id + '_' + pick.player_id} colSpan={1} ><DraftPickCard pick={pick}></DraftPickCard></GridItem>;
-          })}
-        </Grid>
-        
-      </div>
-    </div>
-  );
-};
+	if (error) return <div>Failed to load</div>
+	if (!data) return <div>Loading...</div>
+	return (
+		<div>
+			<h1>DraftBoard: {router.query.draft}</h1>
+			<div>
+				<Grid h='200px' templateColumns='repeat(12, 1fr)' gap={1}>
+					{data.map((pick: DraftPick) => {
+						return (
+							<GridItem
+								key={pick.draft_id + "_" + pick.player_id}
+								colSpan={1}
+							>
+								<DraftPickCard pick={pick}></DraftPickCard>
+							</GridItem>
+						)
+					})}
+				</Grid>
+			</div>
+		</div>
+	)
+}
 
-export default DraftBoard;
+export default DraftBoard
